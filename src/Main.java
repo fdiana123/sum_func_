296c1de6eb68476decef4a2301c41f2ed6e096a5
fdiana123 <123403220+fdiana123@users.noreply.github.com>
2023-04-19 14:39:10 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a = sum(6,7,8);
+        System.out.println(a);
+    }
+
+    static int sum(int x, int y, int z) {
+        return x + y +z;
+    }
+}
